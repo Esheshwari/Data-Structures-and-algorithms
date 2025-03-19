de2869beaae2c850f0439c907c8cb3de8963ff23
Esheshwari Kumari <esheshwaritiwari@gmail.com>
2025-03-19 10:42:19 +0530

    program1.py

--- LAB6/program1.py
+++ LAB6/program1.py
@@ -5,21 +5,22 @@ class Node:
         self.name = name
         self.value = value
 
-# Function to print nodes in reverse order without using .reverse()
 def print_reverse(nodes):
-    # Iterate over the list from the last node to the first node
-    for i in range(len(nodes) - 1, -1, -1):#start stop step
-        node = nodes[i]
+    stack = []
+    
+    while nodes:
+        node = nodes.pop()
+        stack.append(node)
+    
+    while stack:
+        node = stack.pop()
         print(f"{node.name}({node.value})", end=" ")
 
-# Create node objects
 a = Node('a', 200)
 b = Node('b', 300)
 c = Node('c', 400)
 d = Node('d', 500)
 
-# Put nodes into a list
 nodes = [a, b, c, d]
 
-# Call the function to print in reverse
 print_reverse(nodes)
